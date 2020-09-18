64fd42b361a62350c42f818f9932a1215fa78595
Li Linhao <lhli@thoughtworks.com>
2020-09-18 20:09:04 +0800

    remove taro-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   verbose: true,
   moduleNameMapper: {
-    // 'react': 'nervjs',
-    // 'react-addons-test-utils': 'nerv-test-utils',
-    // 'create-react-class': 'nerv-create-class',
-    // 'react-dom': 'nervjs',
-    // '@tarojs/taro': '@tarojs/taro-h5',
     '@tarojs/components': '@tarojs/components/dist-h5/react',
-    '\\.(css|less|sass|scss|styl)$': 'identity-obj-proxy',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
